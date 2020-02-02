870a5344fd9ec09990c6bb7717193152847cf74f
Matt Smith <matt@offtopica.uk>
2020-02-02 15:02:50 +0000

    api: Fix error when looking up stats of inactive miner
    Use COALESCE(...) to prevent the sum of shares submitted from being
    null.

--- api/src/main/java/uk/offtopica/moneropool/api/repository/ShareRepository.java
+++ api/src/main/java/uk/offtopica/moneropool/api/repository/ShareRepository.java
@@ -7,13 +7,13 @@ import uk.offtopica.moneropool.api.model.Share;
 
 public interface ShareRepository extends JpaRepository<Share, Long> {
     @Query(
-            value = "SELECT SUM(difficulty) FROM share WHERE created_at > NOW() - (interval '1' second) * ?1",
+            value = "SELECT COALESCE(SUM(difficulty), 0) FROM share WHERE created_at > NOW() - (interval '1' second) * ?1",
             nativeQuery = true
     )
     long findHashesSubmitted(int period);
 
     @Query(
-            value = "SELECT SUM(difficulty) FROM share WHERE miner_id = ?1 AND created_at > NOW() - (interval '1' " +
+            value = "SELECT COALESCE(SUM(difficulty), 0) FROM share WHERE miner_id = ?1 AND created_at > NOW() - (interval '1' " +
                     "second) * ?2",
             nativeQuery = true
     )
