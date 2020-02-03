e865a2009ff36a9a1e9e434832f48470b35b5fc6
Matt Smith <matt@offtopica.uk>
2020-02-03 11:57:05 +0000

    support: Create block-notify script
    When called, send a message via Redis PUBSUB to notify subscribers of
    a new block on the network.

---
+++ support/block-notify.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# block-notify.sh
+#
+# Part of the moneropool project.
+#
+# Usage: block-notify.sh NETWORK BLOCK_HASH
+#
+# Environment variables:
+#  - REDIS_HOST: Hostname of Redis server (default: 127.0.0.1)
+#  - REDIS_PORT: Port of Redis server (default: 6379)
+
+REDIS_HOST=${REDIS_HOST:-127.0.0.1}
+REDIS_PORT=${REDIS_PORT:-6379}
+
+echo "PUBLISH monero.${1}.blocks ${2}" | nc -q0 127.0.0.1 6379
